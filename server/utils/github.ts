2c33ed08a93f990503a1b5da8077bc763b25f31f
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-10 23:27:37 +0530

    feat: add searchGithub function

--- server/utils/github.ts
+++ server/utils/github.ts
@@ -11,3 +11,58 @@ export function useOctokit() {
 
   return _octokit;
 }
+
+const allowedEndpoints = {
+  commits: 'GET /search/commits',
+  issues: 'GET /search/issues',
+  repositories: 'GET /search/repositories',
+  users: 'GET /search/users',
+} as const;
+
+type EndpointType = keyof typeof allowedEndpoints;
+
+export type SearchParams = {
+  endpoint: string;
+  q: string;
+  order?: string;
+  sort?: string;
+  per_page: string;
+};
+
+export const searchGithub = async (
+  endpoint: string,
+  params: Omit<SearchParams, 'endpoint'>
+) => {
+  console.log(
+    'incoming searchGitbub request for endpoint',
+    endpoint,
+    'with params: ',
+    params
+  );
+
+  if (!endpoint || !allowedEndpoints[endpoint as EndpointType]) {
+    throw createError({
+      statusCode: 404,
+      message: 'Endpoint not supported',
+    });
+  }
+
+  const octokit = useOctokit();
+  const endpointToUse = allowedEndpoints[endpoint as EndpointType];
+
+  try {
+    const response = await octokit.request(endpointToUse as string, params);
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// tryGithub(
+//   allowedEndpoints.issues,
+//   'is:pr author:ra-jeev',
+//   'asc',
+//   'updated',
+//   1
+// ).then((data: any) => console.log(JSON.stringify(data, null, 2)));
