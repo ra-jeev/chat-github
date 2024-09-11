b6c82e01b793cfe92a34a6b75e036621ebdc7520
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-11 21:33:56 +0530

    feat: update system prompt and chat api endpoint

--- server/api/chat.post.ts
+++ server/api/chat.post.ts
@@ -1,32 +1,32 @@
-const systemPrompt = `You are an AI assistant capable of searching GitHub using various endpoints. When a user asks for information that requires searching GitHub, use the "searchGithub" function to perform the search. Here's detailed information about the available endpoints and their parameters:
+const systemPrompt = `You are a helpful assistant who helps users find information on GitHub. \
+When a user asks for information use the searchGithub function to perform the search. 
 
-1. Commits (/search/commits):
+Here's detailed information about the available endpoints and their parameters:
+
+1. commits (/search/commits):
   - Sort options: author-date, committer-date
   - Query qualifiers: author, committer, author-name, committer-name, author-email, committer-email, author-date, committer-date, merge, hash, parent, tree, is, user, org, repo, path, language, topic, comments, created, updated, pushed
 
-2. Issues (/search/issues):
+2. issues (/search/issues):
   - Note: This endpoint also searches pull requests
   - Sort options: comments, reactions, reactions-+1, reactions--1, reactions-smile, reactions-thinking_face, reactions-heart, reactions-tada, interactions, created, updated
   - Query qualifiers: type, is, state, author, assignee, mentions, team, commenter, involves, linked, milestone, project, status, labels, created, updated, closed, comments, user, repo, org, interactions, reactions, draft, review, reviewed-by, review-requested, team-review-requested, file
+  - You must use the "is" qualifier (is:issue or is:pr) to search for issues or pull requests
 
-3. Repositories (/search/repositories):
+3. repositories (/search/repositories):
   - Sort options: stars, forks, help-wanted-issues, updated
   - Query qualifiers: user, org, repo, topic, language, size, followers, forks, stars, created, pushed, updated, mirror, archived, is, license, has
 
-4. Users (/search/users):
+4. users (/search/users):
   - Sort options: followers, repositories, joined
   - Query qualifiers: type, in, repos, location, language, created, followers, fullname, repo, user, org
 
-When using the "searchGithub" function:
+For using the searchGithub function, you should:
 1. Determine the appropriate search endpoint based on the user's request.
-2. Formulate a search query (q) that best matches the user's request, using the relevant qualifiers (do not add unnecessary qualifiers).
+2. Formulate a search query (q) that best matches the user's request (do not add unnecessary qualifiers).
 3. Add any relevant sort, order, or per_page parameters if needed.
-4. Call the "searchGithub" function with the appropriate arguments.
 
-Example:
-User: "Find the top 5 most starred JavaScript repositories on GitHub."
-
-You should form the arguments for the "searchGithub" function like this:
+For example, If the user wants to find the top 5 most starred JavaScript repositories, the following arguments would be used:
 {
   "endpoint": "repositories",
   "q": "language:javascript",
@@ -35,26 +35,32 @@ You should form the arguments for the "searchGithub" function like this:
   "per_page": 5
 }
 
-Then, summarize the results for the user in a clear and concise manner.
-
-Note: The currently logged in github username is "ra-jeev", use this username if the user is talking about themselves.`;
+Then, summarize the results for the user in a clear and concise manner. Use markdown formatting and include the relevant GitHub link as per the user input.`;
 
 export default defineEventHandler(async (event) => {
+  const userSession = await requireUserSession(event);
+
   const { messages } = await readBody(event);
   if (!messages) {
     throw createError({
       statusCode: 400,
-      message: 'Messages are required',
+      message: 'User message is required',
     });
   }
 
-  messages.unshift({
-    role: 'system',
-    content: systemPrompt,
-  });
+  const loggedInUserName = `\n\nNote: The currently logged in github user is "${userSession.user.login}".`;
+  const llmMessages = [
+    {
+      role: 'system',
+      content: systemPrompt + loggedInUserName,
+    },
+    ...messages,
+  ];
 
   try {
-    return await handleMessageWithGroq(messages);
+    const llmResponse = await handleMessageWithGroq(llmMessages);
+
+    return { role: 'assistant', content: llmResponse ?? '' };
   } catch (error) {
     console.error(error);
     throw createError({
