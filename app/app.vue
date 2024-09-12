360182faff1ea72295fd53c84660fa2ff8762fda
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-12 22:53:48 +0530

    feat: watch logout for redirecting to homepage

--- app/app.vue
+++ app/app.vue
@@ -5,9 +5,16 @@
 </template>
 
 <script setup lang="ts">
+const { loggedIn } = useUserSession();
 useHead({
   bodyAttrs: {
     class: 'bg-white dark:bg-gray-900',
   },
+});
+
+watchEffect(() => {
+  if (!loggedIn.value) {
+    navigateTo('/');
+  }
 });
 </script>
