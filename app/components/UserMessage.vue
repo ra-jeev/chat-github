1096f7d3e7db72c83bccfef28093e7b5100380f3
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-18 23:19:12 +0530

    feat: create separate UserMessage component

---
+++ app/components/UserMessage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="flex items-start gap-x-4 p-4 rounded-lg bg-primary-50/15 dark:bg-primary-800/15"
+  >
+    <UAvatar v-if="user && user.avatarUrl" :src="user.avatarUrl" size="lg" />
+    <div v-else class="w-12 h-12 p-2 rounded-full bg-primary/25">
+      <UIcon
+        name="i-heroicons-user-16-solid"
+        class="w-8 h-8 text-primary-500"
+      />
+    </div>
+
+    {{ content }}
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ content: string }>();
+
+const { user } = useUserSession();
+</script>
