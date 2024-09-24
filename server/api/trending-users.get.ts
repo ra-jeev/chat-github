939de196835ad0c87a76b71ac806084833ee8c06
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-24 23:50:29 +0530

    feat: add get trending-users endpoint

---
+++ server/api/trending-users.get.ts
@@ -0,0 +1,12 @@
+export default defineCachedEventHandler(
+  async () => {
+    const results = await getTrendingUsers();
+
+    return {
+      results,
+    };
+  },
+  {
+    maxAge: 10 * 60, // 10 minutes
+  }
+);
