8c863b2facb359cc081e820521a53dcc5ed22456
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-24 23:50:50 +0530

    feat: add get recent-queries endpoint

---
+++ server/api/recent-queries.get.ts
@@ -0,0 +1,12 @@
+export default defineCachedEventHandler(
+  async () => {
+    const results = await getRecentQueries();
+
+    return {
+      results,
+    };
+  },
+  {
+    maxAge: 10 * 60, // 10 minutes
+  }
+);
