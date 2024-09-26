5b0d8215d579f74d70df0adedb10cdeda90d87e4
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-26 16:16:59 +0530

    chore: update type definitions

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,23 @@ export type Message = {
   role: string;
   content: string;
 };
+
+export type SearchParams = {
+  endpoint: string;
+  q: string;
+  order?: string;
+  sort?: string;
+  per_page: string;
+};
+
+export type ToolCallDetails = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  response: any;
+  request: SearchParams;
+};
+
+export type UserQuery = {
+  userMessage: string;
+  toolCalls: ToolCallDetails[];
+  assistantReply: string;
+};
