b513d1a5e405a9bb18bb07bdabc628f1668f9290
Rajeev R Sharma <i.rarsh@gmail.com>
2024-09-27 21:59:06 +0530

    feat: make trending users list github links

--- app/components/TrendingUsers.vue
+++ app/components/TrendingUsers.vue
@@ -3,22 +3,28 @@
     <li
       v-for="user in trendingUsers"
       :key="user.username"
-      class="flex items-center px-3.5 py-2.5 gap-x-3 rounded hover:bg-white/80 dark:hover:bg-gray-900/80"
+      class="rounded hover:bg-white/80 dark:hover:bg-gray-900/80"
     >
-      <UAvatar :src="user.avatar_url" :alt="user.username" size="sm" />
-      <div class="flex-grow">
-        <div class="flex justify-between items-baseline">
-          <p class="text-sm font-medium">
-            {{ user.username }}
-          </p>
+      <ULink
+        class="flex items-center px-3.5 py-2.5 gap-x-3"
+        target="_blank"
+        :to="`https://github.com/${user.username}`"
+      >
+        <UAvatar :src="user.avatar_url" :alt="user.username" size="sm" />
+        <div class="flex-grow">
+          <div class="flex justify-between items-baseline">
+            <p class="text-sm font-medium">
+              {{ user.username }}
+            </p>
+            <p class="text-xs text-gray-500 dark:text-gray-400">
+              {{ user.search_count }} times
+            </p>
+          </div>
           <p class="text-xs text-gray-500 dark:text-gray-400">
-            {{ user.search_count }} times
+            Last searched {{ formatTime(user.last_searched) }}
           </p>
         </div>
-        <p class="text-xs text-gray-500 dark:text-gray-400">
-          Last searched {{ formatTime(user.last_searched) }}
-        </p>
-      </div>
+      </ULink>
     </li>
   </ul>
 </template>
